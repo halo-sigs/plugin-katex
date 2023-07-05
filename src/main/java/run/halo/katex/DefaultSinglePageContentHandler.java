fd427bbe498e062f6d50d49ac843c095d37cf45f
Ryan Wang <i@ryanc.cc>
2023-07-05 10:41:18 +0800

    feat: support adding katex scripts to the post content (#1)
    * feat: support adding katex scripts to the post content
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>
    
    * Add ExtensionDefinition
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>
    
    * chore: update halo api
    
    ---------
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ src/main/java/run/halo/katex/DefaultSinglePageContentHandler.java
@@ -0,0 +1,38 @@
+package run.halo.katex;
+
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import run.halo.app.theme.ReactiveSinglePageContentHandler;
+
+@Component
+public class DefaultSinglePageContentHandler implements ReactiveSinglePageContentHandler {
+
+    @Override
+    public Mono<SinglePageContentContext> handle(SinglePageContentContext singlePageContentContext) {
+
+        String katexScript = """
+            <link rel="stylesheet" href="/plugins/plugin-katex/assets/static/katex.min.css">
+            <script defer src="/plugins/plugin-katex/assets/static/katex.min.js"></script>
+            <script defer src="/plugins/plugin-katex/assets/static/contrib/auto-render.min.js"></script>
+            <script>
+                document.addEventListener("DOMContentLoaded", function() {
+                    renderMathInElement(document.body, {
+                      // customised options
+                      // • auto-render specific keys, e.g.:
+                      delimiters: [
+                          {left: '$$', right: '$$', display: true},
+                          {left: '$', right: '$', display: false},
+                          {left: '\\(', right: '\\)', display: false},
+                          {left: '\\[', right: '\\]', display: true}
+                      ],
+                      // • rendering keys, e.g.:
+                      throwOnError : false
+                    });
+                });
+            </script>
+            """;
+
+        singlePageContentContext.setContent(katexScript + "\n" + singlePageContentContext.getContent());
+        return Mono.just(singlePageContentContext);
+    }
+}
