5160cb36e214a176e7e65f6cf55579700c638f63
1152958806@qq.com <1152958806@qq.com>
2023-09-23 18:53:13 +0800

    解决pjax问题

--- src/main/java/run/halo/katex/KaTeXJSInjector.java
+++ src/main/java/run/halo/katex/KaTeXJSInjector.java
@@ -4,21 +4,27 @@ public class KaTeXJSInjector {
     static String getParsedKatexScript(String inline_selector, String display_selector) {
         String katexScript = """
             <link rel="stylesheet" href="/plugins/plugin-katex/assets/static/katex.min.css">
-            <script defer src="/plugins/plugin-katex/assets/static/katex.min.js"></script>
+            <script defer data-pjax src="/plugins/plugin-katex/assets/static/katex.min.js"></script>
             <script>
-                document.addEventListener("DOMContentLoaded", function() {
-                  const postBody = document.body
-                  const renderMath = (selector,displayMode) => {
-                    const els = postBody.querySelectorAll(selector)
-                    els.forEach((el) => {
-                      katex.render(el.innerText, el, { displayMode })
-                    })
-                  }
-                  if (postBody) {
-                    renderMath("%s",false);
-                    renderMath("%s",true);
-                  }
-                });
+                   function initKatex() {
+                       const postBody = document.body
+                       const renderMath = (selector, displayMode) => {
+                           const els = postBody.querySelectorAll(selector)
+                           els.forEach((el) => {
+                               katex.render(el.innerText, el, { displayMode })
+                           })
+                       }
+                       if (postBody) {
+                           renderMath("%s", false);
+                           renderMath("%s", true);
+                       }
+                   }
+                   document.addEventListener("DOMContentLoaded", function () {
+                       initKatex();
+                   });
+                   document.addEventListener("pjax:complete", function () {
+                       initKatex();
+                   });
             </script>
             """;
         return String.format(katexScript, inline_selector, display_selector);
