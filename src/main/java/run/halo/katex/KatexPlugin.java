f10c7701839bc5de81452dc8010545e91e6a0262
Ryan Wang <i@ryanc.cc>
2024-07-05 14:56:50 +0800

    chore: cleanup deprecated constructor for base plugin
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/main/java/run/halo/katex/KatexPlugin.java
+++ src/main/java/run/halo/katex/KatexPlugin.java
@@ -1,21 +1,14 @@
 package run.halo.katex;
 
-import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
+
 import run.halo.app.plugin.BasePlugin;
+import run.halo.app.plugin.PluginContext;
 
 @Component
 public class KatexPlugin extends BasePlugin {
 
-    public KatexPlugin(PluginWrapper wrapper) {
-        super(wrapper);
-    }
-
-    @Override
-    public void start() {
-    }
-
-    @Override
-    public void stop() {
+    public KatexPlugin(PluginContext context) {
+        super(context);
     }
 }
