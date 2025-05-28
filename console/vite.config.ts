689d5dc61d07701321f9f3c5e2379d1e1c38a26a
Ryan Wang <i@ryanc.cc>
2025-05-28 20:38:00 +0800

    chore: update project infrastructure
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -4,71 +4,25 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
 import { viteStaticCopy as ViteStaticCopy } from "vite-plugin-static-copy";
+import { HaloUIPluginBundlerKit } from "@halo-dev/ui-plugin-bundler-kit";
 
-const pluginEntryName = "plugin-katex";
-
-// https://vitejs.dev/config/
-export default ({ mode }: { mode: string }) => {
-  const isProduction = mode === "production";
-  const outDir = isProduction
-    ? "../src/main/resources/console"
-    : "../build/resources/main/console";
-
-  return defineConfig({
-    plugins: [
-      Vue(),
-      Icons({ compiler: "vue3" }),
-      ViteStaticCopy({
-        targets: [
-          {
-            src: `./node_modules/katex/dist/*`,
-            dest: "../static",
-          },
-        ],
-      }),
-    ],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-    define: {
-      "process.env": process.env,
-    },
-    build: {
-      outDir,
-      emptyOutDir: true,
-      lib: {
-        entry: "src/index.ts",
-        name: pluginEntryName,
-        formats: ["iife"],
-        fileName: () => "main.js",
-      },
-      rollupOptions: {
-        external: [
-          "vue",
-          "vue-router",
-          "@vueuse/core",
-          "@vueuse/components",
-          "@vueuse/router",
-          "@halo-dev/shared",
-          "@halo-dev/components",
-          "@halo-dev/richtext-editor",
-        ],
-        output: {
-          globals: {
-            vue: "Vue",
-            "vue-router": "VueRouter",
-            "@vueuse/core": "VueUse",
-            "@vueuse/components": "VueUse",
-            "@vueuse/router": "VueUse",
-            "@halo-dev/console-shared": "HaloConsoleShared",
-            "@halo-dev/components": "HaloComponents",
-            "@halo-dev/richtext-editor": "RichTextEditor",
-          },
-          extend: true,
+export default defineConfig({
+  plugins: [
+    Vue(),
+    Icons({ compiler: "vue3" }),
+    ViteStaticCopy({
+      targets: [
+        {
+          src: `./node_modules/katex/dist/*`,
+          dest: "../static",
         },
-      },
+      ],
+    }),
+    HaloUIPluginBundlerKit(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  });
-};
+  },
+});
